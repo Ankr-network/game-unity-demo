ab4420fbe3f9aed0c660e8a71ffce79058151725
LUDO\Ludovic <ludovic.domingues96@gmail.com>
2022-05-31 12:01:35 +0200

    Updated scene with NFT handling

--- Assets/AnkrDemo/DemoScifi/Scripts/Demo_AdsManager.cs
+++ Assets/AnkrDemo/DemoScifi/Scripts/Demo_AdsManager.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using AnkrSDK.Ads.UI;
+using AnkrSDK.Core.Infrastructure;
+using AnkrSDK.Examples.ERC20Example;
+using AnkrSDK.Provider;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class Demo_AdsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<AnkrBannerAdSprite> _worldSpaceAdsList;
+    
+    private IEthHandler _eth;
+    
+    private void Start()
     {
-        
+        var ankrSDK = AnkrSDKFactory.GetAnkrSDKInstance(ERC20ContractInformation.HttpProviderURL);
+        _eth = ankrSDK.Eth;
+        DownloadAd().Forget();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private async UniTaskVoid DownloadAd()
     {
+        var defaultAccount = await _eth.GetDefaultAccount();
+        var testAppId = "e8d0f552-22a5-482c-a149-2d51bace6ccb";
+        var testUnitId = "d396af2c-aa3a-44da-ba17-68dbb7a8daa1";
         
+        foreach (AnkrBannerAdSprite ankrBannerAdSprite in _worldSpaceAdsList)
+        {
+            var requestResult = await AnkrAds.Ads.AnkrAds.DownloadAdData(testAppId, testUnitId, defaultAccount);
+            
+            if (requestResult != null)
+            {
+                await ankrBannerAdSprite.SetupAd(requestResult);
+                ankrBannerAdSprite.TryShow();
+            }
+        }
     }
 }
